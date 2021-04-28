df0be05295bb48e1c4a7ff04a0abbd52225d8b6b
hakimbmkg <hakiem87@gmail.com>
2021-04-28 12:23:23 +0700

    add func of geocoder

--- lib/myrisk/myriskpage.dart
+++ lib/myrisk/myriskpage.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-// import 'package:location/location.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geojson/geojson.dart';
 
 class MyRiskPage extends StatelessWidget {
   @override
@@ -24,33 +27,71 @@ class _MapViewState extends State<MapView> {
   CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
   GoogleMapController mapController;
 
-  // ignore: unused_field
-  final Geolocator _geolocator = Geolocator();
+  Set<Marker> markers = {};
 
+// Permission current location
   Position _currentPosition;
+  String _currentAddress;
 
-  // current location
-  // ignore: unused_element
-  _getCurrentLocation() async {
+// Get CurrentLocation
+  void _getCurrentLocation() async {
     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) async {
       setState(() {
         _currentPosition = position;
-        print('CURRENT POS: $_currentPosition');
+        print('Current Position: $_currentPosition');
         mapController.animateCamera(
           CameraUpdate.newCameraPosition(
             CameraPosition(
-              target: LatLng(position.latitude, position.longitude),
-              zoom: 18.0,
-            ),
+                target: LatLng(position.latitude, position.longitude),
+                zoom: 18.0),
           ),
         );
       });
-      // await _getAddress();
+      await _getAddress();
     }).catchError((e) {
       print(e);
     });
   }
+
+// Get Current address from Locator
+  _getAddress() async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          _currentPosition.latitude, _currentPosition.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        _currentAddress =
+            "${place.subLocality}, ${place.locality}, ${place.subAdministrativeArea}";
+        print('current addres: $_currentAddress');
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+// geojson
+  // final lines = <Polyline>[];
+  // String namesline;
+  // _faultIndonesiaPolyline() async {
+  //   final file = File("assets/data/sesar_pusgen.geojson");
+  //   final features = await featuresFromGeoJsonFile(file);
+  //   for (final feature in features.collection) {
+  //     print("${feature.geometry.geoSerie.name}");
+  //     setState(() {
+  //       namesline = "${feature.geometry.geoSerie.name}";
+  //     });
+  //   }
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+    // _faultIndonesiaPolyline();
+  }
+
+  // widget text on top show information of current position
 
   @override
   Widget build(BuildContext context) {
@@ -64,15 +105,46 @@ class _MapViewState extends State<MapView> {
         body: Stack(
           children: <Widget>[
             GoogleMap(
+              markers: markers != null ? Set<Marker>.from(markers) : null,
               initialCameraPosition: _initialLocation,
               myLocationEnabled: true,
               myLocationButtonEnabled: false,
               mapType: MapType.normal,
               zoomGesturesEnabled: true,
               zoomControlsEnabled: false,
+              // polylines: Set<Polyline>.of(polyline.values),
               onMapCreated: (GoogleMapController controller) {
                 mapController = controller;
               },
+            ),
+            SafeArea(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                    width: width * 0.9,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            '$_currentAddress,$namesline',
+                            style: TextStyle(fontSize: 14),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ),
             SafeArea(
               child: Align(
